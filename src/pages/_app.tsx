e959e837b455ce0defeabcebc0133dfacf6cbea8
vaakian <vaakian@gmail.com>
2023-01-02 19:04:20 +0800

    feat: font & loading spinner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,27 @@ import { type AppType } from "next/app";
 
 import { trpc } from "../utils/trpc";
 
+import { Rubik } from '@next/font/google';
 import "../styles/globals.css";
 
+
+const rubik = Rubik({
+  weight: ['400', '500'],
+  display: 'swap'
+})
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <style global jsx>
+      {`
+      :root {
+        font-family: ${rubik.style.fontFamily};
+      }
+      `}
+    </style>
+    <Component {...pageProps} />
+    </>
+  )
 };
 
 export default trpc.withTRPC(MyApp);
--- src/pages/talk/[talkId].tsx
+++ src/pages/talk/[talkId].tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { useEffect } from "react";
 import Talk from "../../components/Talk";
 import { trpc } from "../../utils/trpc";
+import Link from "next/link";
 
 const TalkPage: NextPage = () => {
   const router = useRouter();
@@ -20,12 +21,23 @@ const TalkPage: NextPage = () => {
     }
   }, [refetch, talkId]);
 
+ 
+  // no talkId provided
   if (!talkId) {
-    return <div>Please provide a talkId.</div>;
+    return (
+      <h1 className="flex flex-col h-screen w-screen items-center justify-center text-center text-4xl text-gray-700">
+        <p>Please provide a valid `
+        <span className="text-stone-500 underline">talkId</span>`</p>
+        <Link className="text-2xl text-gray-400" href={'/'}>{'<= '}Go back</Link>
+      </h1>
+    );
   }
+
+  // still fetch
   if (isFetching) {
-    return <div>Fetching...</div>;
+    return <Loading />;
   }
+
   return (
     <>
       <Head>
@@ -37,5 +49,35 @@ const TalkPage: NextPage = () => {
     </>
   );
 };
+
+function Loading() {
+  // make a nice loading spin using tailwind
+  return (
+    <div
+      role="status"
+      className="flex h-screen w-screen flex-col items-center justify-center"
+    >
+      <h1 className="mb-8 text-4xl text-gray-600">
+        your transcript is on the way...
+      </h1>
+      <svg
+        className="mr-2 inline h-[20vh] w-[20vw] animate-spin fill-red-600 text-gray-200 dark:text-gray-600"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
 
 export default TalkPage;
