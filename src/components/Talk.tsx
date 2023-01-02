175743f938328714acb05bc50abcafd36c1018fc
vaakian <vaakian@gmail.com>
2023-01-02 15:38:56 +0800

    feat: UI & interaction

--- src/components/Talk.tsx
+++ src/components/Talk.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
 import type { BilingualDetail } from "../utils/ted";
 import Paragraphs from "./Paragraphs";
 
 export default function Talk({ data }: { data: BilingualDetail}) {
   return (
     <div className="max-w-3xl flex flex-col mx-auto">
-      <h1 className="text-xl">{data.title}</h1>
+      {/* escape character */}
+      <Link href={"/"}><h1 className="text-xl" dangerouslySetInnerHTML={{__html: data.title ?? ''}}></h1></Link>
       <h2 className="italic">{data.author}</h2>
       <Paragraphs paragraphs={data.paragraphs} />
     </div>
